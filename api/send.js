577be4fa5c7682fc55d8637f90b6fc29ee31cc8b
BSBzip <berkeleyburbank1@gmail.com>
2026-06-17 20:33:07 -0700

    Use service role key in send.js so messages INSERT bypasses overly permissive RLS policy

--- api/send.js
+++ api/send.js
@@ -3,7 +3,7 @@ import { getUser } from './_auth.js';
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_PUBLISHABLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export default async function handler(req, res) {
